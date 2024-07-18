884c6bf0c4428c58bf0c310206d7651c6515db3b
Your Name <prosto4ester@gmail.com>
2024-07-18 17:48:23 +0400

    ГОТОВО убрал лишнее, упростил

--- app/src/main/java/com/example/vk_intership_app/ExchangeRatesResponse.kt
+++ app/src/main/java/com/example/vk_intership_app/ExchangeRatesResponse.kt
@@ -1,10 +1,7 @@
 package com.example.vk_intership_app
 
 data class ExchangeRatesResponse (
-    val success: Boolean,
-    val timestamp: Long,
     val base: String,
-    val date: String,
     val rates: Map<String, Double>
 
 )
