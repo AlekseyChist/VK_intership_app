3ee2023ce99fc24aa0bb1936a5c9a6dd7e3c5bb3
Your Name <prosto4ester@gmail.com>
2024-07-18 18:13:51 +0400

    ГОТОВО, добавил корутин на конвертирование

--- app/src/main/java/com/example/vk_intership_app/MainViewModel.kt
+++ app/src/main/java/com/example/vk_intership_app/MainViewModel.kt
@@ -1,8 +1,9 @@
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.vk_intership_app.RetrofitClient
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
-
+import kotlinx.coroutines.launch
 
 
 class MainViewModel : ViewModel() {
@@ -13,15 +14,19 @@ class MainViewModel : ViewModel() {
     private val cacheExpiration = 60 * 60 * 1000 // 1 час в миллисекундах
 
     suspend fun convertCurrency(amount: Double, from: String, to: String) {
-        _conversionResult.value = ConversionResult.Loading
-        try {
-            val rates = getRates("EUR") // Базовая валюта всегда EUR для бесплатного плана
-            val euroRate = if (from == "EUR") 1.0 else 1 / (rates[from] ?: throw Exception("Rate not found for $from"))
-            val targetRate = rates[to] ?: throw Exception("Rate not found for $to")
-            val result = amount * euroRate * targetRate
-            _conversionResult.value = ConversionResult.Success(result)
-        } catch (e: Exception) {
-            _conversionResult.value = ConversionResult.Error("Ошибка конвертации: ${e.localizedMessage}")
+        viewModelScope.launch {
+            _conversionResult.value = ConversionResult.Loading
+            try {
+                val rates = getRates("EUR") // Базовая валюта всегда EUR для бесплатного плана
+                val euroRate = if (from == "EUR") 1.0 else 1 / (rates[from]
+                    ?: throw Exception("Rate not found for $from"))
+                val targetRate = rates[to] ?: throw Exception("Rate not found for $to")
+                val result = amount * euroRate * targetRate
+                _conversionResult.value = ConversionResult.Success(result)
+            } catch (e: Exception) {
+                _conversionResult.value =
+                    ConversionResult.Error("Ошибка конвертации: ${e.localizedMessage}")
+            }
         }
     }
 
